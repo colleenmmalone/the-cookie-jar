76fd1b54417ea3f16ec69e66dfe4cf5c8a37a661
stitchineer <mjollymalone@mjollymalone.fios-router.home>
2022-01-02 22:23:26 -0500

    the cookie jar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import NavBar from './components/navBar'
 import reportWebVitals from './reportWebVitals';
+import Home from './components/Home';
+import Footbar from './components/Footbar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('navBar')
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('main')
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Footbar />
+  </React.StrictMode>,
+  document.getElementById('footbar')
 );
 
 // If you want to start measuring performance in your app, pass a function
