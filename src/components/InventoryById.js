4bcbac79744611761192ae97e9cde0b345c0f439
stitchineer <stitchineer@yahoo.com>
2022-01-05 16:08:07 -0500

    view inventory by id

--- src/components/InventoryById.js
+++ src/components/InventoryById.js
@@ -1,53 +1,84 @@
 import React from "react";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 //import {useTable} from "react-table";
 import axios from 'axios';
+import ReactDOM from "react-dom";
+
 
 
 
 const InventoryById = (id) => {
-  console.log(id["id"]);
-    const url = 'http://localhost:8081/inventory/' + id["id"]
-  
-    const [data,setData] = useState([])
-    
-   useEffect(() => {
-    axios.get(url).then(response => setData(response.data))
+
+  const [data, setData] = useState({});
+
+
+  const inventoryAPI = 'http://localhost:8081/inventory/'
+  const newAPI = inventoryAPI + id.id;
+
+
+
+  useEffect(() => {
+    axios.get(inventoryAPI + id.id)
+      .then((response => setData(response.data)))
   }, [])
 
+  console.log("data");
   console.log(data);
-    const renderTable = () => {
-    
-     
-       
-        return (
-          <tr>
-            <td>{data.itemid}</td>
-            <td>{data.items}</td>
-            <td>{data.quantity}</td> 
-            <td>{data.price}</td> 
-            <td><img src={require('./pictures/'+data.storeImg)} class="thumb"/></td> 
-          </tr>
-        )
-
-    }
-    
+  console.log(data.itemid + " dada");
+  if (data.itemid == undefined) {
     return (
-      <div>
-        <table id="inventory" class="orderDisplay"> 
-          <thead>
-            <tr>
-              <th>ItemID</th>
-              <th>Items</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>StoreImage</th>
-            </tr>
-          </thead>
-          <tbody>{renderTable()}</tbody>
-        </table>
-      </div>
+      <p>no data found for this item number</p>
     )
+  } else {
+    if (data.storeImg == undefined) {
+      return (
+        <div>
+          <table id="inventory" class="orderDisplay">
+            <thead>
+              <tr>
+                <th>ItemID</th>
+                <th>Items</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>StoreImage</th>
+              </tr>
+            </thead>
+            <tr>
+              <td>{data.itemid}</td>
+              <td>{data.items}</td>
+              <td>{data.quantity}</td>
+              <td>{data.price}</td>
+              <td><img src={require('./pictures/default.jpg')} class="thumb" /></td>
+            </tr>
+          </table>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <table id="inventory" class="orderDisplay">
+            <thead>
+              <tr>
+                <th>ItemID</th>
+                <th>Items</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>StoreImage</th>
+              </tr>
+            </thead>
+            <tr>
+              <td>{data.itemid}</td>
+              <td>{data.items}</td>
+              <td>{data.quantity}</td>
+              <td>{data.price}</td>
+              <td><img src={require('./pictures/' + data.storeImg)} class="thumb" /></td>
+            </tr>
+          </table>
+        </div>
+      )
+    }
   }
 
-  export default InventoryById;
+}
+
+export default InventoryById;
