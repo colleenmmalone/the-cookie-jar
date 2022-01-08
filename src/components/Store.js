29c14010657437b03b2cd04196b44038d562848b
stitchineer <stitchineer@yahoo.com>
2022-01-08 11:35:50 -0500

    employee vs customer shows correct button

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,51 +1,38 @@
-//import { render } from "@testing-library/react";
-//import React, { useState } from "react";
-import InventoryMaster from "./InventoryMaster";
-import UploadAndDisplayImage from "./UploadAndDisplay";
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import GenerateTable from "./GenerateTable";
 import axios from "axios";
+import Button from 'react-bootstrap/Button';
+
+import InventoryMaster from "./InventoryMaster";
+import Cart from './Cart.js';
+
 import "../css/Store.css";
-import matcha from './pictures/cake_matcha.jpg';
-import croissant from "./pictures/croissant.jpg";
-import choc from './pictures/cake_chocolate.jpg';
-import defaultImg from "./pictures/default.jpg";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../css/Cart.css'
-import Button from 'react-bootstrap/Button';
-import Cart from './Cart.js';
-//import UpdateInventory from "./UpdateInventory";
 
-
-
-export default function Store(){
+export default function Store() {
     const loginsAPI = ("http://3.87.75.177:8081/logins/");
-    
-    const [item, setItems] = useState([]);
-
-    const [basket, setBasket] = useState([]);
-
-    const [cartPage, setCartPage] = useState(false);
-
-
     const inventoryUrl = ("http://3.87.75.177:8081/inventory/");
+
+    const [item, setItems] = useState([]);
+    const [basket, setBasket] = useState([]);
+    const [cartPage, setCartPage] = useState(false);
+    const [userStatus, setUserStatus] = useState("");
 
     useEffect(function inventoryFunc() {
         axios.get(inventoryUrl)
-            .then(response=> response)
-            .then(({data: item}) => {
+            .then(response => response)
+            .then(({ data: item }) => {
                 setItems(item)
-            }); 
+            });
     }, []);
- 
 
     // render inventory items
     useEffect(function effectFunction() {
-        axios.get(loginsAPI+"whoisloggedin")
-            .then(response => response) 
-            .then(({data}) => {
-                currentUser(data)})
+        axios.get(loginsAPI + "whoisloggedin")
+            .then(response => response)
+            .then(({ data }) => { currentUser(data) })
+            .then(({ data }) => { setUserStatus = data.status; })
             .catch(err => {
                 console.log("Error occured", err);
 
@@ -53,101 +40,87 @@ export default function Store(){
     }, []);
 
     const addToCart = (el) => {
-        const exist = basket.find(x => x.itemid === el.itemid); 
+        const exist = basket.find(x => x.itemid === el.itemid);
         // checks if item exist, if so increment quantity
         if (exist) {
-            setBasket(basket.map((x) => x.itemid === el.itemid ? {...exist, quantity: exist.quantity +1} : x
-                )
+            setBasket(basket.map((x) => x.itemid === el.itemid ? { ...exist, quantity: exist.quantity + 1 } : x
+            )
             );
         } else {
-        // else add item and set initial quantity to 1
-            setBasket([...basket, {...el, quantity: 1}]);
+            // else add item and set initial quantity to 1
+            setBasket([...basket, { ...el, quantity: 1 }]);
         }
     }
-    
-    let inventoryItem = item.map(function(el) {
-        console.log(el.storeImg);
-        let imgSrc;
-        // console.log(el);
-/*             if (el.items === "matcha cake") {
-                imgSrc = matcha;
-            } else if (el.items === "chocolate cake") {
-                imgSrc= choc;
-            } else if (el.items=== "croissant") {
-                imgSrc = croissant;
-            } else {
-                imgSrc = defaultImg;
-            } */
-        
-            //if statement here about being CS or EMP
-            const isCS = (
-                <Button onClick={()=> {addToCart(el)}} variant="info">Add To Cart</Button>
-            );
 
-            return (
-                <tr key={el.itemid}>
+    let inventoryItem = item.map(function (el) {
+        console.log(userStatus);
+        //if statement here about being CS or EMP
+        //colleen could not get this to render right
+        const isCS = () => {
+            //           if(userStatus === 'EMPLOYEE'){
+            //                  return(<></>);
+            //              }else{
+            //          return(<td><Button onClick={()=> {addToCart(el)}} variant="info">Add To Cart</Button></td>);
+        }
+        //       }
+
+        return (
+            <tr key={el.itemid}>
                 <td>
-                <img className='thumb' src={require('./pictures/'+el.storeImg)} alt="cakeimage"/>
+                    <img className='thumb' src={require('./pictures/' + el.storeImg)} alt="cakeimage" />
                 </td>
                 <td>{el.items}</td>
                 <td>${el.price}</td>
-                <td>{isCS}</td>
-                </tr>
-            )   
-        
-    }) 
+                <td><Button onClick={() => { addToCart(el) }} variant="info">Add To Cart</Button></td>
+            </tr>
+        )
 
-  function currentUser(data){
-    console.log(data.firstName);
-    if(data.firstName === undefined){ //if no one is logged in
-        document.getElementById("thisUser").innerHTML = "";
-    }else{
-        console.log(data.status);
-        if(data.status === 'EMPLOYEE'){
-            isEmployee();
-        }else{
-            isCustomer();
-        }
-    }
-}
-    
-    return(
+    })
+
+    return (
         <>
             <h3 class="pageTitle">Store</h3>
-            <h5 id="thisUser"></h5>
-            <div id="empBtnDiv"></div>
+            <div id="empBtnDiv">
+                <button onClick={() => setCartPage(!cartPage)}>{cartPage ? ("Return to store") : ("Checkout")}</button><br />
+            </div>
+            <br />
             <span id="cart">
                 <table class="table table-sm">
-                        <tbody>
-                          {cartPage ? (<Cart basket={basket}/>) : (inventoryItem)}
-                        </tbody>
-                    </table>
-            </span>    
+                    <tbody>
+                        {cartPage ? (<Cart basket={basket} />) : (inventoryItem)}
+                    </tbody>
+                </table>
+            </span>
         </>
     )
 }
 
-
-function updateStore(){
+function updateStore() {
     ReactDOM.render(
         <React.StrictMode>
-         <InventoryMaster />
+            <InventoryMaster />
         </React.StrictMode>,
         document.getElementById('main')
-      );
+    );
 }
 
-
-function isEmployee(){
+function isEmployee() {
     ReactDOM.render(
         <button id="emp" onClick={updateStore}>Update Inventory</button>,
         document.getElementById('empBtnDiv')
     );
 }
 
-function isCustomer(){
-    ReactDOM.render(
-        <button onClick={() => setCartPage(!cartPage)}>{cartPage ? ("Return to store") :("Checkout")}</button>,
-        document.getElementById('empBtnDiv')
-    );
+function currentUser(data) {
+    console.log(data.firstName);
+    if (data.firstName === undefined) { //if no one is logged in
+        document.getElementById("thisUser").innerHTML = "";
+    } else {
+        console.log(data.status);
+        // setUSerStatus = data.status;
+        if (data.status == 'EMPLOYEE') {
+            isEmployee();
+
+        }
+    }
 }
