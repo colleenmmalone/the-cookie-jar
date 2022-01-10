f669cb58370959dedbbfb5133730262c00c38950
Andy Yuen <yuena2036@gmail.com>
2022-01-10 13:02:29 -0800

    can update but frontend is not rerendering changes unless I refresh page

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -14,69 +14,81 @@ export default function Orders() {
     axios.get(loginsAPI+"whoisloggedin")
         .then(function (response){
           getOrders(response.data);
-            })       
+            })  
 
+    function getOrders(user){
+        if(user.firstName === undefined){
+            document.getElementById("thisUser").innerHTML = "No one is logged in"
+        }
+        document.getElementById("thisUser").innerHTML = user.firstName+" "+user.lastName+" is logged in"; 
+            axios.get(ordersAPI)  
+            .then(function (response){
+                displayOrders(user, response.data);
+            })
+        }
 
-        function getOrders(user){
-            if(user.firstName === undefined){
-                document.getElementById("thisUser").innerHTML = "No one is logged in"
-            }
-            document.getElementById("thisUser").innerHTML = user.firstName+" "+user.lastName+" is logged in"; 
-              axios.get(ordersAPI)  
-              .then(function (response){
-                  displayOrders(user, response.data);
-              })
-            }
+    // tells component to do something after selecting
+    const updateStatus = function(id, status) {
+        axios.put(`http://localhost:8081/orders/updateorder/status=${status}/${id}`)
+            .then(response => setItemStatus(response.data))
+            .catch(err => {
+                console.log("Error updating status", err);
+            })
+    }            
             
-function displayOrders(user, orders){
-    console.log(user);
-    console.log(orders);
+    function displayOrders(user, orders){
+        console.log(user);
+        console.log(orders);
 
-       const element = (
-            <table class="orderDisplay">
-                <tr>
-                    <th>Order#</th>
-                    <th>Total</th>
-                    <th>Status</th>
-                </tr>
-               { orders.map(o => {
-                   if(user.id === o.customer){
-                    return(
-                        <tr>
-                        <td>{o.orderid}</td>
-                        <td>{"$"+o.total}</td>
-                        <td>{o.orderStatus}</td>
-                        </tr>
+        const element = (
+                <table class="orderDisplay">
+                    <tr>
+                        <th>Order#</th>
+                        <th>Total</th>
+                        <th>Status</th>
+                        {user.status === "EMPLOYEE" ? <th>Update Status</th>: ""}
+                    </tr>
+                { orders.map(o => {
+                    if(user.id === o.customer){
+                        return(
+                            <tr>
+                            <td>{o.orderid}</td>
+                            <td>{"$"+o.total}</td>
+                            <td>{o.orderStatus}</td>
+                            </tr>
+                        )
+                } else {
+                    return (
+                        <>
+                            {/* {sortType === "ALL" ? 
+                            <tr>
+                            <td>{o.orderid}</td>
+                            <td>{"$"+o.total}</td>
+                            <td>{o.orderStatus}</td>
+                            </tr>: "" } */}
+                            { sortType === o.orderStatus || sortType === "All" ?
+                            <tr>
+                            <td>{o.orderid}</td>
+                            <td>{"$"+o.total}</td>
+                            <td>{o.orderStatus}</td>
+                            <td>
+                                <select name="itemStatus" onChange={(e) => updateStatus(o.orderid, e.target.value)}>
+                                    <option value="" disabled selected>Update status...</option>
+                                    <option value="Pending">Pending</option>
+                                    <option value="Preparing">Preparing</option>
+                                    <option value="Delivered">Delivered</option>
+
+                                </select>
+                            </td>
+                            </tr> : ""
+                            }
+                        </>  
+
                     )
-               } else {
-                   return (
-                     <>
-                        {/* {sortType === "ALL" ? 
-                         <tr>
-                        <td>{o.orderid}</td>
-                        <td>{"$"+o.total}</td>
-                        <td>{o.orderStatus}</td>
-                        </tr>: "" } */}
-                        { sortType === o.orderStatus || sortType === "All" ?
-                        <tr>
-                        <td>{o.orderid}</td>
-                        <td>{"$"+o.total}</td>
-                        <td>
-                            <select>
-                                <option value={o.orderStatus}>{o.orderStatus}</option>
-                                <option value={o.orderStatus}>Preparing</option>
-                                <option value={o.orderStatus}>Delivered</option>
-                            </select>
-                        </td>
-                        </tr> : ""
-                         }
-                     </>  
-
-                   )
-               }
-                })}
-           </table>
-           );
+                }
+                    })}
+            </table>
+            );
     ReactDOM.render(element, document.getElementById('orderDisplay'));
 }
 
@@ -84,11 +96,12 @@ function displayOrders(user, orders){
         <>
             <h3 class="pageTitle">Orders</h3>
             <h5 id="thisUser"></h5>
+            <span>Sort By: </span>
             <select onChange={(e) => setSortType(e.target.value)}>
                 <option value="All">All</option>
-                <option value="PENDING">Pending</option>
-                <option value="PREPARING">Preparing</option>
-                <option value="DELIVERED">Delivered</option>
+                <option value="Pending">Pending</option>
+                <option value="Preparing">Preparing</option>
+                <option value="Delivered">Delivered</option>
             </select>
             <div id="orderDisplay"></div>
 
